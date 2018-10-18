30c3f61b6260dc9507f9ea4d438381358639e42f
maojianling <ermaomail@qq.com>
2018-10-18 14:25:56 +0800

    add yaml example

---
+++ boofuzz/preprocess/example.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Designed for use with boofuzz v0.0.8
+from boofuzz import *
+
+
+def main():
+    """
+    This example is a very simple FTP fuzzer. It uses no process monitory
+    (procmon) and assumes that the FTP server is already running.
+    """
+    session = Session(
+        target=Target(
+            connection=SocketConnection("192.168.1.159", 80, proto='tcp')))
+
+    s_initialize("user")
+    s_string("USER")
+    s_delim(" ")
+    s_string("anonymous")
+    s_static("\r\n")
+
+    s_byte(123456)
+    s_byte(99999)
+
+    print s_get().render()
+
+    s_initialize("pass")
+    s_string("PASS")
+    s_delim(" ")
+    s_string("james")
+    s_static("\r\n")
+
+    print s_get().render()
+
+    s_initialize("stor")
+    s_string("STOR")
+    s_delim(" ")
+    s_string("AAAA")
+    s_static("\r\n")
+
+    print s_get().render()
+
+    s_initialize("retr")
+    s_string("RETR")
+    s_delim(" ")
+    s_string("AAAA")
+    s_static("\r\n")
+
+    print s_get().render()
+
+    session.connect(s_get("user"))
+    session.connect(s_get("user"), s_get("pass"))
+    session.connect(s_get("pass"), s_get("stor"))
+    session.connect(s_get("pass"), s_get("retr"))
+
+    session.fuzz()
+
+
+if __name__ == "__main__":
+    main()
