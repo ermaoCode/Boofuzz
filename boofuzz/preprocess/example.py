c2c060312c2a96284168cb57c6c84419ca4beac5
maojianling <ermaomail@qq.com>
2018-10-19 16:54:17 +0800

    add yaml preprocessor

--- boofuzz/preprocess/example.py
+++ boofuzz/preprocess/example.py
@@ -1,59 +1,44 @@
 #!/usr/bin/env python
 # Designed for use with boofuzz v0.0.8
 from boofuzz import *
+import yaml
 
+def addPrimitive(primitive):
+    if primitive["type"] == "string":
+        s_string(primitive["value"])
+    elif primitive["type"] == "delim":
+        s_delim(primitive["value"])
+    elif primitive["type"] == "static":
+        s_static(primitive["value"])
 
 def main():
-    """
-    This example is a very simple FTP fuzzer. It uses no process monitory
-    (procmon) and assumes that the FTP server is already running.
-    """
-    session = Session(
-        target=Target(
-            connection=SocketConnection("192.168.1.159", 80, proto='tcp')))
+    f = open("example.yaml")
+    # print f.read()
+    y = yaml.load(f.read())
+    # print y
+    try:
+        session = Session(
+            target=Target(
+                connection=SocketConnection(y["session"]["targetconnection"]["ip"],
+                                            y["session"]["targetconnection"]["port"],
+                                            proto=y["session"]["targetconnection"]["protocol"])))
+        pre_status = ""
 
-    s_initialize("user")
-    s_string("USER")
-    s_delim(" ")
-    s_string("anonymous")
-    s_static("\r\n")
+        for status in y["status"]:
+            s_initialize(status["statusname"])
+            for primitive in status["primitives"]:
+                addPrimitive(primitive)
 
-    s_byte(123456)
-    s_byte(99999)
+            if pre_status == "":
+                session.connect(s_get(status["statusname"]))
+                pre_status = status["statusname"]
+            else:
+                session.connect(pre_status, s_get(status["statusname"]))
+                pre_status = status["statusname"]
 
-    print s_get().render()
-
-    s_initialize("pass")
-    s_string("PASS")
-    s_delim(" ")
-    s_string("james")
-    s_static("\r\n")
-
-    print s_get().render()
-
-    s_initialize("stor")
-    s_string("STOR")
-    s_delim(" ")
-    s_string("AAAA")
-    s_static("\r\n")
-
-    print s_get().render()
-
-    s_initialize("retr")
-    s_string("RETR")
-    s_delim(" ")
-    s_string("AAAA")
-    s_static("\r\n")
-
-    print s_get().render()
-
-    session.connect(s_get("user"))
-    session.connect(s_get("user"), s_get("pass"))
-    session.connect(s_get("pass"), s_get("stor"))
-    session.connect(s_get("pass"), s_get("retr"))
-
-    session.fuzz()
-
+        session.fuzz()
+    except Exception as e:
+        print "error: "+ str(e)
 
 if __name__ == "__main__":
     main()
