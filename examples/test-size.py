f70ecabf8e852a6d8887bb565175e095a38b4dab
ermaomail <ermaomail@qq.com>
2019-04-09 15:24:03 +0800

    modify size block to unfuzzable

--- examples/test-size.py
+++ examples/test-size.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # Designed for use with boofuzz v0.0.8
+import sys
+sys.path.append("..")
 from boofuzz import *
 
 
 def main():
     session = Session(
         target=Target(
-            connection=SocketConnection("192.168.1.186", 502, proto='tcp')))
+            connection=SocketConnection("192.168.1.186", 7000, proto='tcp')))
 
     s_initialize("header")
     if s_block_start("b0"):
         if s_block_start("b1"):
-            s_string("a"*31)    # 2 byte: protocol id
-            s_size("b1", length=4)
+            s_size("b1", length=2)
+            s_string("a"*31)
 
         s_block_end()
     s_block_end()
